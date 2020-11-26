848dbc57e19b585273d79c88d10b3832f3b972a1
aayushsinha01 <aayush.sinha@zestmoney.in>
2020-11-26 13:56:04 +0530

    added new component

--- src/components/CustomBanner/index.js
+++ src/components/CustomBanner/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import './styles.scss';
 
 function index(props) {
-    console.log(props.block.data);
     const data = props.block.data;
-  return (
-    <>
-        <div className="custom-banner">
-            <img src={data.img} />
-            <div className="banner-content">
-                <h3>{data.bannerContent.heading}</h3>
-                <p>
-                    {data.bannerContent.content}
-                </p>
+    return (
+        <>
+            <div className="custom-banner">
+                <img src={data.img} />
+                <div className="banner-content">
+                    <h3>{data.bannerContent.heading}</h3>
+                    <p>
+                        {data.bannerContent.content}
+                    </p>
+                </div>
             </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default index;
